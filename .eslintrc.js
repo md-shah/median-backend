bff76c1fd537b77ad3ed45704bbaa12feec325d6
Muhammed Shah <shah@pop-os.localdomain>
2020-05-23 01:33:11 +0530

    feat(architecture): add code linter and pre-commit hook for lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'airbnb',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
@@ -16,7 +17,29 @@ module.exports = {
     node: true,
     jest: true,
   },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+      node: {}
+    },
+  },
   rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        'js': 'never',
+        'jsx': 'never',
+        'ts': 'never',
+        'tsx': 'never',
+      },
+    ],
+    'import/no-extraneous-dependencies': ['error', {
+      'devDependencies': false,
+      'optionalDependencies': false,
+      'peerDependencies': false,
+      "packageDir": __dirname
+    }],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
