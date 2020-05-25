52a4933c5cdf5eff2e67940e3ef049980f91dda1
Muhammed Shah <muhammedshah@live.in>
2020-05-25 20:06:02 +0530

    feat(architecture): add sequilize-postgres basic db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import AppController from './app.controller';
 import AppService from './app.service';
+// import databaseProviders from './server/database/database.provider';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123456',
+      database: 'blog-nestjs',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/server/database/database.provider.ts
+++ src/server/database/database.provider.ts
@@ -1,1 +1,22 @@
-// Database provider goes here
+import { Sequelize } from 'sequelize-typescript';
+
+const databaseProviders = [
+  {
+    provide: 'SEQUELIZE',
+    useFactory: async () => {
+      const sequelize = new Sequelize({
+        dialect: 'postgres',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: 'password',
+        database: 'nest',
+      });
+      sequelize.addModels([]);
+      await sequelize.sync();
+      return sequelize;
+    },
+  },
+];
+
+export default databaseProviders;
