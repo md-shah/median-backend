20668417f2a86cfeb894194c4d2528b09d214a89
Muhammed Shah <muhammedshah@live.in>
2020-05-25 19:25:27 +0530

    feat(architecture): add swagger for api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import AppModule from './app.module';
+import convertStringToSentenceCase from './helpers/convertToSentenceCase';
+import { name, version, description } from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle(convertStringToSentenceCase(name.replace('-', ' ')))
+    .setDescription(description)
+    .setVersion(version)
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
